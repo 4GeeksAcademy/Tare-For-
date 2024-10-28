eb66435926077de1b27d06e9419c4478e6a65692
Antonio Gomez <totico388@hotmail.com>
2024-10-28 16:51:16 +0000

    For listo

---
+++ .vscode/app.js
@@ -0,0 +1,28 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let lola = ['jogger', 'racoon'];
+let dlfml = ['.com', '.es', '.link', '.pm']
+
+for (let index = 0; index < pronoun.length; index++) {
+
+    for (let ad = 0; ad < adj.length; ad++) {
+
+
+        for (let nou = 0; nou < lola.length; nou++) {
+            for (let dl = 0; dl < dlfml.length; dl++) {
+                console.log(pronoun[index]+ adj[ad]+ lola[nou]+ dlfml[dl]);
+    
+                
+            }
+
+
+        }
+
+        
+    }
+
+    
+}
+
+
+
